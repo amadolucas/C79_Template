7cf33c3e385cd43ac3e80d3ba6f772915e66b75b
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-05 10:47:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                    ];
+
+function getMenu(){
+var htmldata;
+//Completar o código
+}
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+//Completar o código
+
+}
+
+function addTop(){
+//Completar o código
+}
